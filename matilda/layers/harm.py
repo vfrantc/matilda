0e4fb8e2fede3d905e8a665722e18e3f5c6c3476
franz.va@yandex.ru <franz.va@yandex.ru>
2020-07-03 13:49:02 -0400

    chebychev harmonic layer

--- matilda/layers/harm.py
+++ matilda/layers/harm.py
@@ -3,6 +3,77 @@ import tensorflow as tf
 from matilda.transform import make_filter_bank
 
 
+class ChebychevTransform(tf.keras.layers.Layer):
+    '''Performs orthogonal transform'''
+
+    def __init__(self, n=4, level=None, strides=(1, 1, 1, 1), padding='SAME', **kwargs):
+        super().__init__(**kwargs)
+        self._n = n
+        self._level = level
+        self._strides = strides
+        self._padding = padding
+        if level is None:
+            self._indexes = None
+        else:
+            self._indexes = [i*n+j for i, j in level]
+
+    def build(self, input_shape):
+        self.built = True
+
+    def call(self, x_input, training=False):
+        batch_size, im_height, im_width, im_channels = x_input.shape.as_list()
+        if batch_size is None:
+            batch_size = -1
+        features = self.extract_cheb_features(x_input,
+                                              N=self._n,
+                                              M=self._n,
+                                              strides=self._strides,
+                                              padding=self._padding.upper())
+        if self._indexes is not None:
+            out_dim = im_channels * len(self._indexes)
+            features = tf.reshape(features, batch_size, im_height, im_width, im_channels, self._n*self._n)
+            features = tf.gather(features, indices=self._indexes, axis=-1)
+        else:
+            out_dim = im_channels * self._n * self._n
+
+        return tf.reshape(features, (batch_size, im_height, im_width, out_dim))
+
+    @tf.function
+    def extract_cheb_features(self, image, N, M, strides=(1, 1, 1, 1), padding='SAME'):
+        '''Based on: https://www.mathworks.com/matlabcentral/fileexchange/4416-2d-chebyshev-transform'''
+        # Takes as an input image: batch_size x height x width x channels
+        # Forms blocks of NxM and performs chebychev transform on them
+        batch_size, im_height, im_width, im_channels = image.shape.as_list()
+        if batch_size is None:
+            batch_size = -1
+
+        # Extract patches of size NxM
+        patches = tf.image.extract_patches(image, sizes=(1, N, M, 1), strides=strides, rates=(1, 1, 1, 1),
+                                           padding='SAME')
+        patches = tf.reshape(patches, [-1, im_height, im_width, N, M, im_channels])
+
+        # form the tensor of patches to transform (last two dimensions are dimensions of patches)
+        patches = tf.transpose(patches, [0, 1, 2, 5, 4, 3])
+        patches = tf.cast(patches, tf.complex64)
+
+        # Chebychev transform using inverse fourier transform
+        F = tf.signal.ifft(tf.concat([patches, patches[..., (N - 2):0:-1]], axis=-1))
+        B = tf.concat([tf.expand_dims(F[..., 0], axis=5), 2 * F[..., 1:N]], axis=5)
+        G = tf.transpose(B, [0, 1, 2, 3, 5, 4])
+        F = tf.signal.ifft(tf.concat([G, G[..., (M - 2):0:-1]], axis=-1))
+
+        return tf.math.real(tf.transpose(tf.concat([tf.expand_dims(F[..., 0], axis=5), 2 * F[..., 1:M]], axis=5), [0, 1, 2, 3, 5, 4]))
+
+    def get_config(self):
+        config = super().get_config()
+        config.update({'n': self._n,
+                       'level': self._level,
+                       'strides': self._strides,
+                       'padding': self._padding})
+        return config
+
+
+
 class SummationHarmonicLayer(tf.keras.layers.Layer):
   def __init__(self, ftype='dct', size=4):
     super(SummationHarmonicLayer, self).__init__()
